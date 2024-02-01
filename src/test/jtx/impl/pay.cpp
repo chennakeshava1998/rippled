01fdbf43813e9adafb4041e7fb58a38c22e0fe4b
Chenna Keshava <ckeshavabs@gmail.com>
2024-02-01 10:06:20 -0800

    unit tests: Payment transactions do not contain the Account field, they only contain the DeliverMax field

--- src/test/jtx/impl/pay.cpp
+++ src/test/jtx/impl/pay.cpp
@@ -31,7 +31,7 @@ pay(AccountID const& account, AccountID const& to, AnyAmount amount)
     amount.to(to);
     Json::Value jv;
     jv[jss::Account] = to_string(account);
-    jv[jss::Amount] = amount.value.getJson(JsonOptions::none);
+    jv[jss::DeliverMax] = amount.value.getJson(JsonOptions::none);
     jv[jss::Destination] = to_string(to);
     jv[jss::TransactionType] = jss::Payment;
     jv[jss::Flags] = tfUniversal;
